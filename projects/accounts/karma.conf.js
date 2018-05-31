588efa22d22d0310bc6f691f4859020bc020067b
edward sharp <edward@edwardsharp.net>
2018-05-31 02:59:47 -0400

    wait for it…

--- projects/accounts/karma.conf.js
+++ projects/accounts/karma.conf.js
@@ -26,6 +26,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
-    singleRun: true
+    singleRun: true,
+    captureTimeout: 240000
   });
 };
